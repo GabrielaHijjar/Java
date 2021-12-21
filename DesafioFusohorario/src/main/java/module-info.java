ad418611279842129820c50ee2d4ceb1dc7d5448
GabrielaHijjar <gf.hijjar@gmail.com>
2021-12-21 16:58:54 -0300

    desafios

--- DesafioFusohorario/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.desafiofusohorario {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.desafiofusohorario to javafx.fxml;
-    exports com.example.desafiofusohorario;
-}
--- desafiodama/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.desafiodama {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.desafiodama to javafx.fxml;
-    exports com.example.desafiodama;
-}
