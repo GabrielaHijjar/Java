9be8cb1f1f61ed8e9bcb2b2a66f000e70bc90670
GabrielaHijjar <gf.hijjar@gmail.com>
2021-12-21 16:39:29 -0300

    desafios

--- desafioXadrez/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.desafioxadrez {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.desafioxadrez to javafx.fxml;
-    exports com.example.desafioxadrez;
-}
