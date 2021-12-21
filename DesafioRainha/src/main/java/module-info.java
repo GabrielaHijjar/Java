cf11b166306728f1d4ad885f2cf97b23cc6a268c
GabrielaHijjar <gf.hijjar@gmail.com>
2021-12-21 16:48:42 -0300

    rainha

--- DesafioRainha/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.desafiorainha {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.desafiorainha to javafx.fxml;
-    exports com.example.desafiorainha;
-}
