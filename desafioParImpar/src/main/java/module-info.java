7ae5acc7fe5d604048c5e603399d3c3e053ef730
GabrielaHijjar <gf.hijjar@gmail.com>
2021-12-21 16:44:18 -0300

    alteração

--- desafioParImpar/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.desafioparimpar {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.desafioparimpar to javafx.fxml;
-    exports com.example.desafioparimpar;
-}
