c08776d08f4249368770a28825f1d72553da2ba7
GabrielaHijjar <gf.hijjar@gmail.com>
2022-03-02 15:03:19 -0300

    DesafioVariavel

---
+++ VariavelSimOUNao/src/main/java/com/example/variavelsimounao/variavel.java
@@ -0,0 +1,35 @@
+
+
+//Paulinho tem em suas mãos um pequeno problema. A professora lhe pediu que ele construísse um programa para verificar,
+// à partir de dois valores inteiros A e B, se B corresponde aos últimos dígitos de A.
+// Entrada
+// A entrada consiste de vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade
+// de casos de teste. Cada caso de teste consiste de dois inteiros A (1 ≤ A < 231 ) e B (1 ≤ B < 231) positivos.
+//Saída
+// Para cada caso de entrada imprima uma mensagem indicando se o segundo valor encaixa no primeiro valor, confome exemplo abaixo
+
+
+
+package com.example.variavelsimounao;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class variavel {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        for (int i = 0; i < N; i++) {
+
+            String A = leitor.next();
+            String B = leitor.next();
+
+            if (A.endsWith(B))
+                System.out.println("encaixa");
+            else
+                System.out.println("nao encaixa");
+        }
+    }
+
+}
