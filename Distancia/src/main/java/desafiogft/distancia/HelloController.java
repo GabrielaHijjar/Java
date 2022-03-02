622840863f69c4c79a3f120683515e1c988bbf58
GabrielaHijjar <gf.hijjar@gmail.com>
2022-03-02 14:48:11 -0300

    DesafioGFT--- Distancia

--- Distancia/src/main/java/desafiogft/distancia/HelloController.java
+++ Distancia/src/main/java/desafiogft/distancia/HelloController.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-//
-Desafio
-
-        Duas motos (X e Y) partem em uma mesma direção. A moto X sai com velocidade constante de 60 Km/h e a moto Y sai com velocidade constante de 90 Km/h.
-        Em uma hora (60 minutos) a moto Y consegue se distanciar 30 quilômetros da moto X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
-        O seu desafio é ler a distância (em Km) e calcular quanto tempo leva (em minutos) para a moto Y tomar essa distância da outra moto.        Entrada
-
-        O arquivo de entrada contém um número inteiro K que representa a quantidade de quilômetro que que a moto Y deve estar da moto X.
-        Saída
-
-        Imprima o tempo necessário para a moto Y ficar com a quantidade K de quilômetro da moto X, seguido da mensagem " minutos".
-//
-
-
-
-
-        package desafiogft.distancia;
-=======
 
 
 //Duas motos (X e Y) partem em uma mesma direção. A moto X sai com velocidade constante de 60 Km/h e a moto Y sai com velocidade constante de 90 Km/h.
@@ -29,7 +10,7 @@ Desafio
 
 
 package desafiogft.distancia;
->>>>>>> 3608aab12f6d53e6ef39a22aa5051598f781d3cd
+
 
 import java.util.Scanner;
 
