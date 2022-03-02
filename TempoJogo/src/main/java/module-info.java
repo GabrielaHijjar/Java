0598ab615b87991faa60bd283b4603fb03249dab
GabrielaHijjar <gf.hijjar@gmail.com>
2022-03-02 15:37:51 -0300

    DesafioGFT

--- TempoJogo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tempojogo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tempojogo to javafx.fxml;
-    exports com.example.tempojogo;
-}
--- multiplos/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.multiplos {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.multiplos to javafx.fxml;
-    exports com.example.multiplos;
-}
