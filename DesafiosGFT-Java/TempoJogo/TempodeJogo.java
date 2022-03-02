8a1cff5fceb82546d96a216ddb2fee53fe195b0f
Gabriela Hijjar <gf.hijjar@gmail.com>
2022-03-02 15:45:24 -0300

    Update TempodeJogo.java

--- DesafiosGFT-Java/TempoJogo/TempodeJogo.java
+++ DesafiosGFT-Java/TempoJogo/TempodeJogo.java
@@ -1,3 +1,17 @@
+
+//Desafio
+
+//Tento como base a hora inicial e final de um jogo, calcule a duração do dele, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de, 60 minutos e máxima de 24 horas.
+//Entrada
+//A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
+//Saída
+//Apresente a duração do jogo conforme exemplo abaixo.
+ 
+//Exemplo de Entrada 	Exemplo de Saída
+
+//16 2
+//O JOGO DUROU 10 HORA(S)
+
 package com.example.tempojogo;
 
 
