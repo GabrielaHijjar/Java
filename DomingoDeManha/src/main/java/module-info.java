2dab0a45a698fcb9c719777bbe816653c1604a73
GabrielaHijjar <gf.hijjar@gmail.com>
2022-03-05 22:39:50 -0300

    Desafios

--- DomingoDeManha/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.domingodemanha {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.domingodemanha to javafx.fxml;
-    exports com.example.domingodemanha;
-}
