5e5d3c556dcc11f96fdddfb661b37d1acf555d2c
GabrielaHijjar <gf.hijjar@gmail.com>
2022-03-05 22:14:09 -0300

    Desafio de Java Domingo de Manhã

---
+++ DomingoDeManha/src/main/java/com/example/domingodemanha/Problem.java
@@ -0,0 +1,31 @@
+package com.example.domingodemanha;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Problem {
+
+        public static void main(String[] args) throws IOException {
+                Scanner leitor = new Scanner(System.in);
+                while (leitor.hasNext()) {
+                        String[] relogio = leitor.nextLine().split(":");
+                        int hora = Integer.parseInt(relogio[0]);
+                        int min = Integer.parseInt(relogio[1]);
+                        switch (hora) {
+                                case (7):
+                                        System.out.println("Atraso maximo: " + min);
+                                        break;
+                                case (8):
+                                        System.out.println("Atraso maximo: " + (60 + min));
+                                        break;
+                                case (9):
+                                        System.out.println("Atraso maximo: " + (120 + min));
+                                        break;
+                                default:
+                                        System.out.println("Atraso maximo: 0");
+                        }
+                }
+
+
+        }
+}
